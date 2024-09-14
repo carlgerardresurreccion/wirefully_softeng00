a7e56ee03ce1ea6ccd83c82cee6c0bb96f2c6b82
Alyssa Vivien S. Cañas <alyssavivien.sc@gmail.com>
2024-09-14 19:44:19 +0800

    1

--- src/JS/App.js
+++ src/JS/App.js
@@ -2,14 +2,13 @@ import '../CSS/App.css';
 import { BrowserRouter as Routes, Route } from 'react-router-dom';
 import Dashboard from './Dashboard';
 import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
-import ProtectedRoute from './ProtectedRoute';
 
 function App() {
   return (
     <BrowserRouter>
       <div className='MainPage'>
         <Routes>
-            <ProtectedRoute exact path="/" component={Dashboard}/>
+            <Dashboard/>
         </Routes>
       </div>
     </BrowserRouter>
--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './JS/App';
 import reportWebVitals from './reportWebVitals';
-import { AuthProvider } from './JS/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AuthProvider>
       <App />
-    </AuthProvider>
   </React.StrictMode>
 );
 
